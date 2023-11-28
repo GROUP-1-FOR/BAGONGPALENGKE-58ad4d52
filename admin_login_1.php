f883718cacdecb795f067a2c481ee4f3540e736c
francisgoree <gorreonfrancis@gmail.com>
2023-11-28 23:00:30 +0800

    Only accepts hashed password for more password security

--- admin_login_1.php
+++ admin_login_1.php
@@ -13,15 +13,16 @@ if (isset($_POST["admin_login_submit"])) {
     } else {
         $admin_userid = htmlspecialchars($_POST["admin_userid"]);
         $admin_password = htmlspecialchars($_POST["admin_password"]);
+        $hashedPassword = md5($admin_password);
 
-        $result = mysqli_query($connect, "SELECT * FROM admin_sign_in WHERE admin_userid= '$admin_userid' AND admin_password='$admin_password'");
+        $result = mysqli_query($connect, "SELECT * FROM admin_sign_in WHERE admin_userid= '$admin_userid'");
         $row = mysqli_fetch_assoc($result);
 
 
         if (mysqli_num_rows($result) > 0) {
 
 
-            if ($admin_password == $row["admin_password"]) {
+            if ($hashedPassword == $row["admin_password"]) {
                 $_SESSION["login"] = true;
                 $_SESSION["id"] = $row["admin_id"];
                 $_SESSION["userid"] = $row["admin_userid"];
