8e8f62410a5a2081ac8cc85fca29b7f7cf859524
Jheg Ardales <jhegardales2002@gmail.com>
2023-12-02 17:26:55 +0800

    CSS
    CSS

--- admin_index.php
+++ admin_index.php
@@ -6,24 +6,43 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
 
 
     //to know last log in time of admin
-    include('admin_login_time.php');
+    //include('admin_login_time.php');
 
 ?>
     <!DOCTYPE html>
     <html>
 
     <head>
-        <title>Main Page</title>
-    </head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> SIGN IN </title>
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
 
     <body>
+
+    <div class="container">
+    <div class="sidebar">
+        <h2>Sidebar Title</h2>
+        <p>Some sidebar content goes here.</p>
+        <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+        </ul>
+    </div>
+    </div>
+    
+
+<div class="content">
+
         <h1>Welcome, <?php echo $admin_userid ?>! </h1>
-
-
         <a href=admin_confirmpay.php>
-            <h1>Confirm Payment</h1>
+        <h1>Confirm Payment</h1>
         </a>
-
         <a href=''>
             <h1>View Market Rental Stalls</h1>
         </a>
@@ -48,8 +67,10 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
         <a href=admin_logout.php>
             <h1>LOGOUT</h1>
         </a>
+        </div>
+        </div>
+  
     </body>
-
 
     </html>
 <?php } else {
--- admin_login.php
+++ admin_login.php
@@ -8,56 +8,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> ADMIN SIGN IN </title>
-
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SIGN IN</title>
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <!-- External stylesheet link -->
+<body class="bagongpgalengke-v2" >
+  <header> LOGO </header>
 
-    <header class=>
-        <h2> LOGO </h2>
+  <div class="website-title-v2">
+    <h1 class="title3"> WELCOME TO </h1>
+    <h1 class="title4"> SANTA ROSA<br>PUBLIC MARKET</h1>
+  </div>
 
-        <h1 class="title1"> WELCOME TO </h1>
-        <h1 class="title2"> SANTA ROSA PUBLIC MARKET </h1>
-        <!--<div class="nav">
-      <a href="">HOME</a>
-      <a href="">BLOG</a>
-      <a href="">About</a>
-      <a href="">FAQs</a>
-      <a href="">Info</a>
-      <a href="">Tricks</a>
-      <a href="">LOGIn</a>
+  <div>
+    <img class= "white-front" src="assets\images\sign-in\white-front.svg" alt="white-front">
+    <img class= "front-layer-v2" src="assets\images\sign-in\front.svg" alt="front">
+    <img class= "back-layer-v2" src="assets\images\sign-in\back.svg" alt="back">
+  </div>
 
-    </div> -->
-    </header>
+<div class="login-form">
+  <h2>LOGIN</h2>
 
-    <div class="main-content">
-
-        <form class="" action="admin_login_1.php" method="post" autocomplete="off">
-            <label for="Admin User ID"> Admin User ID: </label>
-            <input type="text" name="admin_userid" id="admin_userid" required value=""><br />
-            <label for="Password"> Password: </label>
-            <input type="password" name="admin_password" id="admin_password" required value=""><br />
-            <button class="signin-button" type="submit" name="admin_login_submit">Enter</button>
-        </form>
-        <a href="admin_forgot_password.php"> Forgot Password?</a> <br />
-        <a href="vendor_admin_select.php"> Back</a>
-
-    </div>
-
-    <footer>
-
-    </footer>
-    <main>
-
-    </main>
+      <form class="form-group" action="admin_login_1.php" method="post" autocomplete="off">
+          <label for="Admin User ID"> Admin User ID: </label>
+          <input type="text" name="admin_userid" id="admin_userid" required value=""> <br />
+          <label for="Password"> Password: </label>
+          <input type="password" id="password" placeholder="PASSWORD" name="admin_password" id="admin_password" required value=""><br />
+          <button class="login-btn" type="submit" name="admin_login_submit"> LOGIN </button>
+      </form>
+      <a class ="forgot-password" href="admin_forgot_password.php"> Forgot Password?</a> <br />
+      <a class="back-button" href="vendor_admin_select.php"> Back</a>
+  </div>
+  <footer> </footer>
 </body>
+
 
 </html>
