2e82687915290c629042e49cbb65c38b9667f1ff
francisgoree <gorreonfrancis@gmail.com>
2023-12-03 16:46:07 +0800

    Edited button so vendor works

--- vendor_admin_select.php
+++ vendor_admin_select.php
@@ -18,17 +18,18 @@
     <h1 class="title2"> SANTA ROSA PUBLIC MARKET </h1>
   </div>
   <div>
-  <img class="tech-line-tr" src="assets\\images\\sign-in\\tech-line-tr.png" alt="tech-line-tr">
-  <img class="tech-line-bl" src="assets\\images\\sign-in\\tech-line-bl.png" alt="tech-line-bl">
-  <img class="front-layer" src ="assets\\images\\sign-in\\front.svg" alt="front-layer">
-  <img class="back-layer" src ="assets\\images\\sign-in\\back.svg" alt="back-layer">
-</div>
-<div
-  <a href="vendor_login.php"><button class="vendor-button"> VENDOR </button></a>
-  <a href="admin_login.php"><button class="admin-button"> ADMIN </button></a>
-  <a href="first_signin.php"><button class="back-button"> &lt; BACK </button></a>
-</div
+    <img class="tech-line-tr" src="assets\\images\\sign-in\\tech-line-tr.png" alt="tech-line-tr">
+    <img class="tech-line-bl" src="assets\\images\\sign-in\\tech-line-bl.png" alt="tech-line-bl">
+    <img class="front-layer" src="assets\\images\\sign-in\\front.svg" alt="front-layer">
+    <img class="back-layer" src="assets\\images\\sign-in\\back.svg" alt="back-layer">
+  </div>
+  <div>
+    <a href="vendor_login.php"><button class="vendor-button"> VENDOR </button></a>
+    <a href="admin_login.php"><button class="admin-button"> ADMIN </button></a>
+    <a href="first_signin.php"><button class="back-button"> &lt; BACK </button></a>
+  </div>
   <section> </section>
   <footer> </footer>
 </body>
+
 </html>
