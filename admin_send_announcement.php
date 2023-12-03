ccfb0174a423452ae997fc4dd0521eaa9ebd1df2
francisgoree <gorreonfrancis@gmail.com>
2023-12-03 21:09:29 +0800

    Removed See all announcements and moved it into the send announcement page

--- admin_send_announcement.php
+++ admin_send_announcement.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require("config.php");
 if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["userid"])) {
     $admin_id = $_SESSION["id"];
@@ -20,6 +19,32 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
     <body>
 
         <h1>Welcome, <?php echo $admin_userid ?>! </h1>
+        <?php
+        $sql_sent_announcement = "SELECT DISTINCT announcement_text, announcement_time FROM announcements ORDER BY announcement_id DESC";
+        $result_sent_announcement = $connect->query($sql_sent_announcement);
+
+        $connect->close();
+
+
+        if ($result_sent_announcement->num_rows > 0) {
+            while ($row = $result_sent_announcement->fetch_assoc()) {
+                // Display sent announcements
+                echo "<p>Announcement: " . $row['announcement_text'] . "</p>";
+                echo "<p>Date and Time: " . $row['announcement_time'] . "</p><hr>";
+                echo "<hr />";
+            }
+        } else {
+            echo "<p>No sent announcements found.</p>";
+        }
+
+        ?>
+
+
+
+
+
+
+
         <h1>Send an Announcement:</h1>
 
         <form action="admin_send_announcement_1.php" method="post">
@@ -29,10 +54,6 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
             <textarea name="admin_announcement" id="admin_announcement" cols="30" rows="5" required></textarea><br>
             <input type="submit" value="Send announcement">
         </form>
-
-        <a href=admin_view_announcement.php>
-            <h1>SEE ALL ANNOUNCEMENTS</h1>
-        </a>
 
         <a href=admin_index.php>
             <h1>BACK</h1>
