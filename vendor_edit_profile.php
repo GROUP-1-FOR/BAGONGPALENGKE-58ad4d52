a08b2b5e75df934aa76dd61e1a2e7fa60a9547bc
Hezeria <gracesantos0875@gmail.com>
2023-12-04 20:07:44 +0800

    vendor profile

---
+++ vendor_edit_profile.php
@@ -0,0 +1,291 @@
+<?php
+require("config.php");
+if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["userid"])) {
+    $admin_id = $_SESSION["id"];
+    $admin_userid = $_SESSION["userid"];
+
+    function generateUserID($pdo)
+    {
+        while (true) {
+            // Generate a random 5-digit number
+            $randomNumber = str_pad(rand(0, 99999), 5, '0', STR_PAD_LEFT);
+
+            // Form the user ID
+            $userID = "VSR-" . $randomNumber;
+
+            // Check if the user ID is unique in the database
+            if (isUniqueUserID($pdo, $userID)) {
+                return $userID;
+            }
+        }
+    }
+
+    // Function to check if the generated user ID is unique in the database
+    function isUniqueUserID($pdo, $userID)
+    {
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM vendor_sign_in WHERE vendor_userid = ?");
+        $stmt->execute([$userID]);
+
+        return $stmt->fetchColumn() == 0;
+    }
+
+    // Create  Vendor User ID
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=bagong_palengke_db", "root", "");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+
+
+?>
+
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Create Vendor Account</title>
+        <script>
+            function validateVendorFirstName() {
+                var vendor_name = document.getElementById("vendor_first_name").value;
+                var vendor_name_error_span = document.getElementById("vendor_first_name_error_span");
+
+                // Check if the input contains any numbers or symbols
+                if (vendor_name.length > 0 && /[0-9!@#$%^&*(),.?":{}|<>]/.test(vendor_name)) {
+                    vendor_name_error_span.textContent = "Please enter the vendor name without numbers or symbols.";
+                    return false; // Prevent form submission
+                } else {
+                    vendor_name_error_span.textContent = "";
+                }
+
+                // If the input is valid, you can proceed with form submission
+                return true;
+            }
+
+            function validateVendorLastName() {
+                var vendor_name = document.getElementById("vendor_last_name").value;
+                var vendor_name_error_span = document.getElementById("vendor_last_name_error_span");
+
+                // Check if the input contains any numbers or symbols
+                if (vendor_name.length > 0 && /[0-9!@#$%^&*(),.?":{}|<>]/.test(vendor_name)) {
+                    vendor_name_error_span.textContent = "Please enter the vendor name without numbers or symbols.";
+                    return false; // Prevent form submission
+                } else {
+                    vendor_name_error_span.textContent = "";
+                }
+
+                // If the input is valid, you can proceed with form submission
+                return true;
+            }
+
+
+            function validateVendorMobileNumber() {
+                var vendor_mobile_number = document.getElementById("vendor_mobile_number").value;
+                var vendor_mobile_number_error_span = document.getElementById("vendor_mobile_number_error_span");
+
+                // Check if the input contains any non-numeric characters
+                if (vendor_mobile_number.length > 0 && !/^09\d{9}$/.test(vendor_mobile_number)) {
+                    // Show error message
+                    vendor_mobile_number_error_span.textContent = "Please enter a valid mobile number.";
+                    return false; // Prevent form submission
+                } else {
+                    // Clear error message
+                    vendor_mobile_number_error_span.textContent = "";
+                }
+
+                // If the input is valid, you can proceed with form submission
+                return true;
+            }
+
+            function validateVendorEmail() {
+                var vendor_email = document.getElementById("vendor_email").value;
+                var vendor_email_error_span = document.getElementById("vendor_email_error_span");
+
+                // Use the built-in email validation
+                if (vendor_email.length > 0 && !document.getElementById("vendor_email").checkValidity()) {
+                    // Show error message
+                    vendor_email_error_span.textContent = "Please enter a valid email address.";
+                    return false; // Prevent form submission
+                } else {
+                    // Clear error message
+                    vendor_email_error_span.textContent = "";
+                }
+
+                // If the input is valid, you can proceed with form submission
+                return true;
+            }
+
+            function checkPasswordMatch() {
+                var password = document.getElementsByName("vendor_password")[0].value;
+                var confirmPassword = document.getElementsByName("vendor_confirm_password")[0].value;
+                var messageElement = document.getElementById("passwordMatchMessage");
+                var confirmPasswordInput = document.getElementsByName("vendor_confirm_password")[0];
+
+                // Enable or disable Confirm Password based on whether Password is empty
+                confirmPasswordInput.disabled = password.length === 0;
+
+                // Check if the "Password" field is not empty
+                if (password.length > 0) {
+                    // Check if the "Confirm Password" field is also not empty
+                    if (confirmPassword.length > 0) {
+                        // Check if passwords match
+                        if (password === confirmPassword) {
+                            // Check if passwords have at least 8 characters
+                            if (password.length >= 8 && confirmPassword.length >= 8) {
+                                messageElement.innerHTML = "Passwords match and meet the minimum length requirement.";
+                                messageElement.style.color = "green";
+                            } else {
+                                messageElement.innerHTML = "Passwords match but do not meet the minimum length requirement (8 characters).";
+                                messageElement.style.color = "red";
+                            }
+                        } else {
+                            messageElement.innerHTML = "Passwords do not match.";
+                            messageElement.style.color = "red";
+                        }
+                    } else {
+                        // "Confirm Password" field is empty, clear the message
+                        messageElement.innerHTML = "";
+                    }
+                } else {
+                    // "Password" field is empty, clear the message and "Confirm Password" field
+                    messageElement.innerHTML = "";
+                    confirmPasswordInput.value = "";
+                }
+
+                return password === confirmPassword && password.length >= 8 && confirmPassword.length >= 8;
+            }
+
+
+
+
+            function updateSubmitButton() {
+                var submitButton = document.querySelector('button[type="submit"]');
+
+
+                var formIsValid = validateVendorFirstName() && validateVendorLastName() && validateVendorMobileNumber() && validateVendorEmail() && checkPasswordMatch();
+                submitButton.disabled = !formIsValid;
+            }
+        </script>
+
+    </head>
+
+    <body>
+
+        <h1>Create Vendor Account, <?php echo $admin_userid  ?>! </h1>
+
+        <form action="admin_create_vendor_account_1.php" method="post" onsubmit="return validateForm()">
+
+
+            <h2>Vendor Information</h2>
+
+            <label for="Vendor First Name">Vendor First Name</label>
+            <input type="text" name="vendor_first_name" id="vendor_first_name" required oninput="validateVendorFirstName(); updateSubmitButton()">
+            <!-- Display an error message if it exists in the session -->
+            <span style="color: red;" id="vendor_first_name_error_span">
+
+                <?php
+                if (isset($_SESSION['vendor_first_name_error'])) {
+                    echo $_SESSION['vendor_first_name_error'];
+                    // Unset the session variable after displaying the error
+                    unset($_SESSION['vendor_first_name_error']);
+                }
+                ?>
+            </span>
+            <br />
+            <label for="Vendor Last Name">Vendor Last Name</label>
+            <input type="text" name="vendor_last_name" id="vendor_last_name" required oninput="validateVendorLastName(); updateSubmitButton()">
+            <!-- Display an error message if it exists in the session -->
+            <span style="color: red;" id="vendor_last_name_error_span">
+
+                <?php
+                if (isset($_SESSION['vendor_last_name_error'])) {
+                    echo $_SESSION['vendor_last_name_error'];
+                    // Unset the session variable after displaying the error
+                    unset($_SESSION['vendor_last_name_error']);
+                }
+                ?>
+            </span>
+
+            <br />
+            <label for="Stall Number">Stall No:</label>
+            <input type="number" name="vendor_stall_number" required><br />
+
+            <label for="Mobile Number">Mobile Number</label>
+            <input type="tel" name="vendor_mobile_number" id="vendor_mobile_number" maxlength="11" placeholder="09XXXXXXXXX" oninput="validateVendorMobileNumber(); updateSubmitButton()">
+            <!-- Display an error message if it exists in the session -->
+            <span style="color: red;" id="vendor_mobile_number_error_span">
+                <?php
+                if (isset($_SESSION['vendor_mobile_number_error'])) {
+                    echo $_SESSION['vendor_mobile_number_error'];
+                    // Unset the session variable after displaying the error
+                    unset($_SESSION['vendor_mobile_number_error']);
+                }
+                ?>
+            </span>
+
+            <br />
+            <label for="Email">Email:</label>
+            <input type="email" name="vendor_email" id="vendor_email" required oninput="validateVendorEmail(); updateSubmitButton()">
+            <!-- Display an error message if it exists in the session -->
+            <span style="color: red;" id="vendor_email_error_span">
+                <?php
+                if (isset($_SESSION['vendor_email_error'])) {
+                    echo $_SESSION['vendor_email_error'];
+                    // Unset the session variable after displaying the error
+                    unset($_SESSION['vendor_email_error']);
+                }
+                ?>
+            </span>
+
+            <br />
+            <label for="Product Type">Products:</label>
+            <select name="vendor_product" required>
+                <option value="" disabled selected>Select Product Type</option>
+                <option value="Wet">Wet</option>
+                <option value="Dry">Dry</option>
+                <option value="Other">Other</option>
+            </select><br />
+
+            <br />
+            <br />
+
+
+            <h2>Vendor Account</h2>
+            <label>Vendor User ID</label>
+            <input type="text" name="vendor_userid" value="<?php echo $new_vendor_userid = generateUserID($pdo); ?>" readonly><br />
+
+            <label>Password</label>
+            <input type="password" name="vendor_password" placeholder="8 characters and above" oninput="checkPasswordMatch(); updateSubmitButton()"><br />
+
+            <label>Confirm Password</label>
+            <input type="password" name="vendor_confirm_password" required oninput="checkPasswordMatch(); updateSubmitButton()">
+            <span id="passwordMatchMessage"></span><br />
+
+
+
+            <button type="submit" disabled>Submit</button>
+        </form>
+
+        <script>
+            function validateForm() {
+                return validateVendorFirstName() && validateVendorLastName() && validateVendorMobileNumber() && validateVendorEmail() && checkPasswordMatch();
+            }
+        </script>
+
+
+
+        <a href=admin_vendor_manage_accounts.php>
+            <h1>BACK</h1>
+        </a>
+
+        <a href=admin_logout.php>
+            <h1>LOGOUT</h1>
+        </a>
+    </body>
+
+
+    </html>
+<?php } else {
+    header("location:admin_login.php");
+}
---
+++ vendor_edit_profile_1.php
@@ -0,0 +1,87 @@
+<?php
+require("config.php");
+//Error message for the first part of the form
+$vendor_first_name_error  = $vendor_last_name_error  = $vendor_mobile_number_error = $vendor_email_error = "";
+//Error message for the second part
+$vendor_userid_error = $vendor_password_error = $vendor_confirm_password_error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Part 1 variables
+    $vendor_first_name = isset($_POST["vendor_first_name"]) ? htmlspecialchars($_POST["vendor_first_name"]) : '';
+    $vendor_last_name = isset($_POST["vendor_last_name"]) ? htmlspecialchars($_POST["vendor_last_name"]) : '';
+    $vendor_stall_number = isset($_POST["vendor_stall_number"]) ? trim(htmlspecialchars($_POST["vendor_stall_number"])) : '';
+    $vendor_mobile_number = isset($_POST["vendor_mobile_number"]) ? trim(htmlspecialchars($_POST["vendor_mobile_number"])) : '';
+    $vendor_email = isset($_POST["vendor_email"]) ? htmlspecialchars($_POST["vendor_email"]) : '';
+    $vendor_product_type = isset($_POST["vendor_product"]) ? htmlspecialchars($_POST["vendor_product"]) : '';
+
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $vendor_first_name)) {
+        $vendor_first_name_error = "Only letters are allowed";
+        $_SESSION['vendor_first_name_error'] = $vendor_first_name_error;
+    }
+
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $vendor_last_name)) {
+        $vendor_last_name_error = "Only letters are allowed";
+        $_SESSION['vendor_last_name_error'] = $vendor_last_name_error;
+    }
+
+    if (!is_numeric($vendor_mobile_number)) {
+        $vendor_mobile_number_error = "Only numbers are allowed";
+        $_SESSION['vendor_mobile_number_error'] = $vendor_mobile_number_error;
+    }
+    if (!filter_var($vendor_email, FILTER_VALIDATE_EMAIL)) {
+        $vendor_email_error = "Wrong email format";
+        $_SESSION['vendor_email_error'] = $vendor_email_error;
+    }
+
+    if (isset($_SESSION['vendor_first_name_error']) || isset($_SESSION['vendor_last_name_error']) || isset($_SESSION['vendor_mobile_number_error']) || isset($_SESSION['vendor_email_error'])) {
+        header("Location: admin_create_vendor_account.php");
+        exit();
+    }
+
+
+    // Part2 variables
+    $vendor_userid = isset($_POST["vendor_userid"]) ? htmlspecialchars($_POST["vendor_userid"]) : '';
+    $vendor_password = isset($_POST["vendor_password"]) ? htmlspecialchars($_POST["vendor_password"]) : '';
+    $vendor_confirm_password = isset($_POST["vendor_confirm_password"]) ? htmlspecialchars($_POST["vendor_confirm_password"]) : '';
+
+
+    if ($vendor_password !== $vendor_confirm_password) {
+        echo '<script>';
+        echo 'alert("Passwords do not match!");';
+        echo 'window.location.href = "admin_create_vendor_account.php";';
+        echo '</script>';
+        exit();
+    }
+
+    // Hash the password
+    $hashedPassword = md5($vendor_password);
+    $vendor_full_name = $vendor_first_name . " " . $vendor_last_name;
+
+    // Check if the account already exists
+    $checkIfExists = "SELECT * FROM vendor_sign_in WHERE vendor_userid = '$vendor_userid'";
+    $resultIfExists = $connect->query($checkIfExists);
+
+    if ($resultIfExists->num_rows > 0) {
+        echo '<script>';
+        echo 'alert("Account already exists for this user ID!");';
+        echo 'window.location.href = "admin_create_vendor_account.php";';
+        echo '</script>';
+        exit();
+    }
+
+    // Perform database insertion (you might need to adjust table/column names)
+    $sql = "INSERT INTO vendor_sign_in (vendor_name, vendor_stall_number,vendor_mobile_number,vendor_product,vendor_email, vendor_userid, vendor_password, balance) 
+        VALUES ('$vendor_full_name', '$vendor_stall_number','$vendor_mobile_number','$vendor_product_type','$vendor_email', '$vendor_userid', '$hashedPassword', '0.00')";
+
+    if ($connect->query($sql) === TRUE) {
+        echo '<script>';
+        echo 'alert("Vendor Account Created Successfully!");';
+        echo 'window.location.href = "admin_index.php";';
+        echo '</script>';
+    } else {
+        echo "Error: " . $sql . "<br>" . $connect->error;
+    }
+
+    // Close the database connection
+    $connect->close();
+}
