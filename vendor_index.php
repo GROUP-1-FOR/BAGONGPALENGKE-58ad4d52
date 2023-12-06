4819d5f1402daa6e7d87b23dea718f84651bc462
Hezeria <gracesantos0875@gmail.com>
2023-12-06 11:09:44 +0800

    dito na muna habang di pa ayos main

--- vendor_index.php
+++ vendor_index.php
@@ -3,8 +3,8 @@ require("config.php");
 if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["userid"])) {
     $id = $_SESSION["id"];
     $userid = $_SESSION["userid"];
-     //to know last log in time of vendor
-     include('vendor_login_time.php');
+    //to know last log in time of vendor
+    include('vendor_login_time.php');   
     // Fetch user data using prepared statement
     $sqlUserData = "SELECT * FROM vendor_sign_in WHERE vendor_userid = ?";
     $stmtUserData = $connect->prepare($sqlUserData);
@@ -22,64 +22,56 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
         die("User not found or database query issue.");
     }
 
-// Check the payment status
-$paymentStatus = "To be paid";
+    // Check the payment status
+    $paymentStatus = "To be paid";
 
-// Check if the payment has been sent but not confirmed
-$sqlCheckPayment = "SELECT * FROM ven_payments WHERE id = ? AND name = ? AND confirmed = 0 AND archived = 0";
-$stmtCheckPayment = $connect->prepare($sqlCheckPayment);
-$stmtCheckPayment->bind_param('is', $userid, $vendorName);
-$stmtCheckPayment->execute();
-$resultCheckPayment = $stmtCheckPayment->get_result();
+    // Check if the payment has been sent but not confirmed
+    $sqlCheckPayment = "SELECT * FROM ven_payments WHERE id = ? AND name = ? AND confirmed = 0 AND archived = 0";
+    $stmtCheckPayment = $connect->prepare($sqlCheckPayment);
+    $stmtCheckPayment->bind_param('is', $userid, $vendorName);
+    $stmtCheckPayment->execute();
+    $resultCheckPayment = $stmtCheckPayment->get_result();
 
-if ($resultCheckPayment->num_rows > 0) {
-    $paymentStatus = "Payment has already been sent";
-}
+    if ($resultCheckPayment->num_rows > 0) {
+        $paymentStatus = "Payment has already been sent";
+    }
 
-// Check if the payment is confirmed
-$sqlCheckPaymentConfirmation = "SELECT * FROM ven_payments WHERE id = ? AND name = ? AND confirmed = 1 AND archived = 1";
-$stmtCheckPaymentConfirmation = $connect->prepare($sqlCheckPaymentConfirmation);
-$stmtCheckPaymentConfirmation->bind_param('is', $userid, $vendorName);
-$stmtCheckPaymentConfirmation->execute();
-$resultCheckPaymentConfirmation = $stmtCheckPaymentConfirmation->get_result();
+    // Check if the payment is confirmed
+    $sqlCheckPaymentConfirmation = "SELECT * FROM ven_payments WHERE id = ? AND name = ? AND confirmed = 1 AND archived = 1";
+    $stmtCheckPaymentConfirmation = $connect->prepare($sqlCheckPaymentConfirmation);
+    $stmtCheckPaymentConfirmation->bind_param('is', $userid, $vendorName);
+    $stmtCheckPaymentConfirmation->execute();
+    $resultCheckPaymentConfirmation = $stmtCheckPaymentConfirmation->get_result();
 
-if ($resultCheckPaymentConfirmation->num_rows > 0) {
-    $paymentStatus = "The payment is confirmed";
-    // Set balance to 0 as payment is confirmed
-    $balance = $row['balance'];
-}
+    if ($resultCheckPaymentConfirmation->num_rows > 0) {
+        $paymentStatus = "The payment is confirmed";
+        // Set balance to 0 as payment is confirmed
+        $balance = $rowUserData['balance'];
+    }
 
-    // Process payment if the "Pay" button is clicked
-    if (isset($_POST['pay']) && $paymentStatus === "To be paid" && $balance > 0) {
-       
-        // Insert payment data into ven_payments table
-        $paymentDate = date('Y-m-d H:i:s');
-        $sqlInsertPayment = "INSERT INTO ven_payments (id, name, balance, archived, confirmed, payment_date) VALUES (?, ?, ?, 0, 0, ?)";
-        $stmtInsertPayment = $connect->prepare($sqlInsertPayment);
-        $stmtInsertPayment->bind_param('ssds', $userid, $vendorName, $balance, $paymentDate); // Use 's' for VARCHAR and 'd' for DOUBLE
-        $stmtInsertPayment->execute();
-
-        // Set payment status to "Payment request sent" in session
-       // $_SESSION['payment_status'] = "Payment request sent. Please wait for confirmation.";
-
-        // Redirect to the same page to avoid form resubmission on refresh
-        header("Location: ".$_SERVER['PHP_SELF']);
-        exit();
-    } 
     // Check for payment status in session and reset the session variable
     if (isset($_SESSION['payment_status'])) {
         echo $_SESSION['payment_status'];
         unset($_SESSION['payment_status']);
     }
-    
-
+    // Check if the conditions allow redirecting to the invoice summary
+    if (isset($_POST['pay']) && $paymentStatus === "To be paid" && $balance > 0) {
+        // Check if there is an unconfirmed payment before redirecting
+        if ($resultCheckPayment->num_rows === 0) {
+            header("Location: vendor_invoice_summary.php?vendorName=$vendorName&vendorUserId=$userid&vendorStallNumber=$stallNumber&balance=$balance");
+            exit();
+        } else {
+            echo "Cannot redirect to invoice summary because payment has already been sent but not confirmed.";
+        }
+    }
 ?>
-    <!DOCTYPE html>
-    <html>
 
-    <head>
-        <title>Main Page</title>
-        <style>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Main Page</title>
+    <style>
         body {
             text-align: center;
             margin: 50px;
@@ -98,7 +90,8 @@ if ($resultCheckPaymentConfirmation->num_rows > 0) {
             padding: 50px;
             background-color: #850F16;
             color: white;
-            font-size: 2em; /* Adjust the font size as needed */
+            font-size: 2em;
+            /* Adjust the font size as needed */
         }
 
         #money-cell button {
@@ -111,36 +104,42 @@ if ($resultCheckPaymentConfirmation->num_rows > 0) {
             cursor: pointer;
         }
     </style>
-    </head>
+</head>
 
-    <body>
+<body>
     <h1><?php echo "Hi, " . $vendorName; ?>!</h1>
-        <!-- Display vendor information -->
-        <p>Stall No: <?php echo $stallNumber; ?></p>
-        <p>Vendor ID: <?php echo $userid; ?></p>
+    <!-- Display vendor information -->
+    <p>Stall No: <?php echo $stallNumber; ?></p>
+    <p>Vendor ID: <?php echo $userid; ?></p>
 
     <!-- Vendor Pay Table -->
     <table id="money-table">
-        <tr>
-            <td id="money-cell">
-                <center>
-                    <?php if ($balance > 0): ?>
-                        $<?php echo number_format($balance, 2); ?>
-                        <form method="post">
+    <tr>
+        <td id="money-cell">
+            <center>
+                <?php if ($balance > 0): ?>
+                    $<?php echo number_format($balance, 2); ?>
+                    <?php if ($paymentStatus === "To be paid"): ?>
+                        <form method="post" action="vendor_invoice_summary.php">
+                            <input type="hidden" name="vendorName" value="<?php echo $vendorName; ?>">
+                            <input type="hidden" name="vendorUserId" value="<?php echo $userid; ?>">
+                            <input type="hidden" name="vendorStallNumber" value="<?php echo $stallNumber; ?>">
+                            <input type="hidden" name="balance" value="<?php echo $balance; ?>">
                             <button type="submit" name="pay" onclick="return confirm('Are you sure you want to make the payment?')">Pay</button>
                         </form>
-                    <?php else: ?>
-                        $<?php echo number_format($balance, 2); ?>
                     <?php endif; ?>
-                    <?php if ($paymentStatus === "Payment has already been sent"): ?>
-                        <p>Payment has already been sent. Wait for confirmation.</p>
-                    <?php elseif ($paymentStatus === "The payment is confirmed"): ?>
-                        <p>The payment is confirmed. You have no current balance.</p>
-                    <?php endif; ?>
-                </center>
-            </td>
-        </tr>
-    </table>
+                <?php else: ?>
+                    $<?php echo number_format($balance, 2); ?>
+                <?php endif; ?>
+                <?php if ($paymentStatus === "Payment has already been sent"): ?>
+                    <p>Payment has already been sent. Wait for confirmation.</p>
+                <?php elseif ($paymentStatus === "The payment is confirmed"): ?>
+                    <p>The payment is confirmed. You have no current balance.</p>
+                <?php endif; ?>
+            </center>
+        </td>
+    </tr>
+</table>
     <br>
     <a href=vendor_edit_profile.php>
         <h1>EDIT PROFILE</h1>
@@ -157,8 +156,9 @@ if ($resultCheckPaymentConfirmation->num_rows > 0) {
     </a>
 </body>
 
-
-    </html>
-<?php } else {
+</html>
+<?php
+} else {
     header("location:vendor_login.php");
 }
+?>
--- vendor_invoice_summary.php
+++ vendor_invoice_summary.php
@@ -1,37 +1,131 @@
+<?php
+// vendor_invoice_summary.php
+
+// Include the configuration file
+require("config.php");
+
+// Check if the necessary POST data is set
+if (isset($_POST['vendorName']) && isset($_POST['vendorUserId']) && isset($_POST['vendorStallNumber']) && isset($_POST['balance'])) {
+    // Get data from POST variables
+    $vendorName = $_POST['vendorName'];
+    $vendorUserId = $_POST['vendorUserId'];
+    $vendorStallNumber = $_POST['vendorStallNumber'];
+    $balance = $_POST['balance'];
+} else {
+    // Redirect to the main page if data is not set
+    header("Location: vendor_index.php");
+    exit();
+}
+
+// Check if the "Cash" button is clicked
+if (isset($_POST['pay_cash'])) {
+    // Insert payment data into ven_payments table
+    $paymentDate = date('Y-m-d H:i:s');
+    $sqlInsertPayment = "INSERT INTO ven_payments (id, name, balance, archived, confirmed, payment_date) VALUES (?, ?, ?, 0, 0, ?)";
+    $stmtInsertPayment = $connect->prepare($sqlInsertPayment);
+    $stmtInsertPayment->bind_param('ssds', $vendorUserId, $vendorName, $balance, $paymentDate); // Use 's' for VARCHAR and 'd' for DOUBLE
+    $stmtInsertPayment->execute();
+
+    // Set payment status to "Payment request sent" in session
+    // $_SESSION['payment_status'] = "Payment request sent. Please wait for confirmation.";
+
+    // Redirect to the same page to avoid form resubmission on refresh
+    header("Location: ".$_SERVER['PHP_SELF']);
+    exit();
+}
+
+// Check if the "GCash" button is clicked
+if (isset($_POST['pay_gcash'])) {
+    // Insert GCash payment data into ven_payments table (modify as needed)
+    // ...
+    // Redirect to the same page or another page based on your logic
+    // ...
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invoice Summary</title>
+    <style>
+        body {
+            text-align: center;
+            margin: 50px;
+            background-color: #f2f2f2;
+        }
+
+        #invoice-summary {
+            width: 50%;
+            margin: auto;
+            border: 3px solid #ccc;
+            padding: 20px;
+            background-color: #fff;
+        }
+
+        #invoice-summary h2 {
+            color: #850F16;
+        }
+
+        #invoice-summary p {
+            font-size: 1.2em;
+            margin: 10px 0;
+        }
+
+        #payment-buttons {
+            margin-top: 20px;
+        }
+
+        #payment-buttons button {
+            margin: 10px;
+            padding: 10px;
+            background-color: #850F16;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
 </head>
+
 <body>
-
-    <div id="invoice">
+    <div id="invoice-summary">
         <h2>Invoice Summary</h2>
-        <p><strong>Address:</strong> <span id="address">123 Main Street, Cityville</span></p>
-        <p><strong>Name:</strong> <span id="name">John Doe</span></p>
-        <p><strong>Total Balance to be Paid:</strong> $<span id="totalBalance">100.00</span></p>
-        <button onclick="payInvoice()">Pay</button>
+        <p><strong>Vendor Name:</strong> <?php echo $vendorName; ?></p>
+        <p><strong>Vendor ID:</strong> <?php echo $vendorUserId; ?></p>
+        <p><strong>Stall Number:</strong> <?php echo $vendorStallNumber; ?></p>
+        <p><strong>Balance:</strong> $<?php echo number_format($balance, 2); ?></p>
+
+        <!-- Add more invoice details or calculations as needed -->
+
+        <p><strong>Payment Status:</strong> To be paid</p>
+        <!-- ... -->
+
+        <div id="payment-buttons">
+            <!-- Cash Payment Button -->
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <input type="hidden" name="vendorName" value="<?php echo $vendorName; ?>">
+                <input type="hidden" name="vendorUserId" value="<?php echo $vendorUserId; ?>">
+                <input type="hidden" name="vendorStallNumber" value="<?php echo $vendorStallNumber; ?>">
+                <input type="hidden" name="balance" value="<?php echo $balance; ?>">
+                <button type="submit" name="pay_cash" onclick="return confirm('Are you sure you want to pay with cash?')">Pay with Cash</button>
+            </form>
+
+            <!-- GCash Payment Button -->
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <input type="hidden" name="vendorName" value="<?php echo $vendorName; ?>">
+                <input type="hidden" name="vendorUserId" value="<?php echo $vendorUserId; ?>">
+                <input type="hidden" name="vendorStallNumber" value="<?php echo $vendorStallNumber; ?>">
+                <input type="hidden" name="balance" value="<?php echo $balance; ?>">
+                <button type="submit" name="pay_gcash" onclick="return confirm('Are you sure you want to pay with GCash?')">Pay with GCash</button>
+            </form>
+        </div>
+
+        <!-- Add any additional information or details about the invoice -->
+        <!-- ... -->
+
+        <p><a href="vendor_index.php">Back to Main Page</a></p>
     </div>
-
-    <script>
-        // Function to update invoice details dynamically
-        function updateInvoiceDetails(address, name, totalBalance) {
-            document.getElementById('address').innerText = address;
-            document.getElementById('name').innerText = name;
-            document.getElementById('totalBalance').innerText = totalBalance;
-        }
-
-        // Function to handle the payment process
-        function payInvoice() {
-            // Add your payment processing logic here
-            alert('Payment successful!');
-        }
-
-        // Example: Update invoice details
-        updateInvoiceDetails('456 Oak Avenue, Townsville', 'Jane Smith', '150.00');
-    </script>
-
 </body>
+
 </html>
