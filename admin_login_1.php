2096b10f38d0198886fc767eebdee5b5567513e2
Hezeria <gracesantos0875@gmail.com>
2023-12-11 22:51:42 +0800

    login admin 1

--- admin_login_1.php
+++ admin_login_1.php
@@ -14,7 +14,6 @@ if (isset($_POST["admin_login_submit"])) {
     } else {
         $admin_userid = htmlspecialchars($_POST["admin_userid"]);
         $admin_password = htmlspecialchars($_POST["admin_password"]);
-        $hashedPassword = md5($admin_password);
 
         $result = mysqli_query($connect, "SELECT * FROM admin_sign_in WHERE admin_userid= '$admin_userid'");
         $row = mysqli_fetch_assoc($result);
@@ -22,7 +21,7 @@ if (isset($_POST["admin_login_submit"])) {
         if (mysqli_num_rows($result) > 0) {
 
 
-            if ($hashedPassword == $row["admin_password"]) {
+            if (password_verify($admin_password, $row["admin_password"])) {
                 $_SESSION["login"] = true;
                 $_SESSION["id"] = $row["admin_id"];
                 $_SESSION["userid"] = $row["admin_userid"];
