44ba27b8b0c0776aa263e81cdaca063e485d8bc5
Hezeria <gracesantos0875@gmail.com>
2023-12-22 16:23:47 +0800

    remove checkbox

--- admin_vendor_manage_accounts.php
+++ admin_vendor_manage_accounts.php
@@ -117,6 +117,7 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
         <tr>
             <th>Vendor Information</th>
             <th>Action</th>
+            <th>Remove</th> <!-- New column for checkboxes -->
         </tr>
 
         <?php
@@ -140,7 +141,7 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
         // Display data in a table
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
+                echo "<tr data-vendorid='" . $row["vendor_userid"] . "'>";
                 echo "<td>" . $row["vendor_name"] . "<br>Vendor ID: " . $row["vendor_userid"] . "</td>";
                 echo "<td>";
 
@@ -152,21 +153,101 @@ if (isset($_SESSION["id"]) && $_SESSION["login"] === true && isset($_SESSION["us
                 }
 
                 echo "</td>";
+                echo "<td><input type='checkbox' class='vendorCheckbox' name='selectedVendors[]' value='" . $row["vendor_userid"] . "' onchange='handleCheckboxSelection()'></td>"; // Add onchange attribute here
                 echo "</tr>";
             }
         } else {
-            echo "<tr><td colspan='2'>No vendors found</td></tr>";
+            echo "<tr><td colspan='3'>No vendors found</td></tr>";
         }
         ?>
     </table>
 
-    <!--Button to add vendors -->
-    <button id="addButton" onclick="redirectToAddVendors()">Add Vendor (+)</button>
+     <!-- Add Vendor button -->
+     <center>
+    <div id="addButton">
+    <button onclick="redirectToAddVendors()">Add Vendor (+)</button>
+    <!-- Remove Vendor button -->
+    <button id="removeButton" onclick="removeSelectedVendors()" disabled>Remove Vendor</button><br>
+    </div>
 
     <a href=admin_index.php><button id="Button">Back</button></a>
-
+    </center>
+    
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script>
+         // Function to handle the checkbox selection
+         function handleCheckboxSelection() {
+            const checkboxes = document.querySelectorAll('.vendorCheckbox');
+            const removeButton = document.getElementById('removeButton');
+
+            let anyCheckboxSelected = false;
+
+            checkboxes.forEach(checkbox => {
+                if (checkbox.checked) {
+                    anyCheckboxSelected = true;
+                }
+            });
+
+            removeButton.disabled = !anyCheckboxSelected;
+        }
+
+        // Function to handle the Remove Vendor button click
+        function removeSelectedVendors() {
+            const checkboxes = document.querySelectorAll('.vendorCheckbox:checked');
+            const selectedVendorIds = [];
+
+            checkboxes.forEach(checkbox => {
+                selectedVendorIds.push(checkbox.value);
+            });
+
+            // Show a confirmation dialog
+            const confirmRemove = confirm('Are you sure you want to remove the selected vendors?');
+
+            if (confirmRemove) {
+                // Perform the AJAX request or form submission to remove selected vendors
+                // Replace 'remove_vendor.php' with the actual PHP file to handle vendor removal
+                $.ajax({
+                    url: 'remove_vendor.php',
+                    type: 'POST', // Use POST to submit the selected vendor_userids
+                    data: { vendorIds: selectedVendorIds }, // Pass selected vendor IDs to the server
+                    dataType: 'json', // Expect JSON response
+                    success: function(response) {
+                        // Handle the response as needed
+                        if (response.success) {
+                            console.log('Vendors removed successfully');
+                            // Update the table without refreshing the page
+                            updateTable(selectedVendorIds);
+                        } else {
+                            console.error('Error removing vendors:', response.error);
+                        }
+                    },
+                    error: function(error) {
+                        console.error('AJAX error:', error);
+                    }
+                });
+            }
+        }
+
+// Function to dynamically update the table after removing vendors
+function updateTable(selectedVendorIds) {
+    // Remove the corresponding table rows
+    selectedVendorIds.forEach(vendorId => {
+        const rowToRemove = document.querySelector(`[data-vendorid="${vendorId}"]`);
+        if (rowToRemove) {
+            rowToRemove.remove();
+        }
+    });
+
+    // Clear the checkbox selection and disable the Remove button
+    const checkboxes = document.querySelectorAll('.vendorCheckbox');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+
+    const removeButton = document.getElementById('removeButton');
+    removeButton.disabled = true;
+}
+
         function editVendor(vendorId) {
             window.location.href = 'admin_edit_vendor.php?vendor_userid=' + encodeURIComponent(vendorId);
         }
