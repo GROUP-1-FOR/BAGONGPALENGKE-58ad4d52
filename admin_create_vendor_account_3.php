2ea1046e4ea26fdbfe64deb9c54e14a8faac760b
francisgoree <gorreonfrancis@gmail.com>
2024-02-11 20:09:52 +0800

    Fixed the mail style

--- admin_create_vendor_account_3.php
+++ admin_create_vendor_account_3.php
@@ -27,11 +27,12 @@ $mail->Subject = 'Vendor User Account';
 
 
 $mail->addEmbeddedImage('santarosapublicmarket.png', 'logo');
-$mail->Body = '<div style="text-align: center;">
-                  <img src="cid:logo" alt="Santa Rosa Public Market"><br />
-                  Vendor User ID: ' . $vendor_userid . '<br />
-                  Vendor Password: ' . $vendor_password_1 . '<br />
-                  This is your vendor credentials. For your protection, do not share this with anyone.
+$mail->Body = '<div style="text-align: center; font-family: Arial, sans-serif; background-color: #f2f2f2; padding: 20px;">
+                  <img src="cid:logo" alt="Santa Rosa Public Market" style="max-width: 200px;"><br />
+                  <p style="font-size: 18px; color: #333;">Vendor User ID: <strong>' . $vendor_userid . '</strong></p>
+                  <p style="font-size: 18px; color: #333;">Vendor Password: <strong>' . $vendor_password_1 . '</strong></p>
+                  <p style="font-size: 16px; color: #666;">This is your vendor credentials. For your protection, do not share this with anyone.</p>
                </div>';
+
 
 $mail->send();
